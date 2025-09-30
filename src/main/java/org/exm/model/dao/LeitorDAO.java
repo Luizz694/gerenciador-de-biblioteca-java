9ad3f90befeb97af17912c46f48b018bbb379740
José Luiz <157260669+Luizz694@users.noreply.github.com>
2025-09-30 15:49:03 -0300

    <FEAT> : Finalização dos métodos de LeitorDAO

--- src/main/java/org/exm/model/dao/LeitorDAO.java
+++ src/main/java/org/exm/model/dao/LeitorDAO.java
@@ -1,23 +1,24 @@
 package org.exm.model.dao;
 
 import org.exm.model.entity.Leitor;
+import org.exm.model.entity.Livro;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LeitorDAO {
-    public void SalvarLeitor(Leitor leitor){
+    public void SalvarLeitor(Leitor leitor) {
         String sql = "INSERT into leitor(Nome, Email) VALUES (?, ?) ";
 
         try (Connection conn = ConexaoFactory.createConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)){
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, leitor.getNome());
             pstmt.setString(2, leitor.getEmail());
 
             pstmt.executeUpdate();
-        }catch (SQLException e) {
-            throw new RuntimeException("Erro ao salvar o leitor/usuário: "+ e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao salvar o leitor/usuário: " + e);
         }
     }
 
@@ -29,7 +30,7 @@ public class LeitorDAO {
              PreparedStatement pstmt = conn.prepareStatement(sql);
              ResultSet aux = pstmt.executeQuery()) {
 
-            while (aux.next()){
+            while (aux.next()) {
                 int idLeitor = aux.getInt("idLeitor");
                 String Nome = aux.getString("Nome");
                 String Email = aux.getString("Email");
@@ -39,10 +40,60 @@ public class LeitorDAO {
                 leitores.add(leitor);
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao procurar leitores");
         }
 
         return leitores;
+    }
+
+    public void deletarLeitor(int idLeitor) {
+        String sql = "DELETE FROM leitor WHERE idLeitor = ?";
+
+        try (Connection conn = ConexaoFactory.createConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, idLeitor);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Leitor buscarPorId(int idLeitor) {
+        String sql = "SELECT * FROM leitor WHERE idLeitor = ?";
+        Leitor leitor = null;
+
+        try (Connection conn = ConexaoFactory.createConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, idLeitor);
+
+            try (ResultSet aux = pstmt.executeQuery()) {
+                if (aux.next()) {
+                    String Nome = aux.getString("Nome");
+                    String Email = aux.getString("Email");
+
+                    leitor = new Leitor(idLeitor, Nome, Email);
+                }
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return leitor;
+    }
+
+    public void editarLeitor(Leitor leitor){
+        String sql = "UPDATE leitor SET Nome = ?, Email = ?";
+
+        try(Connection conn = ConexaoFactory.createConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, leitor.getEmail());
+            pstmt.setString(2, leitor.getNome());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
